73001c0d9ad25023bb894299ea8d12b6c714af1b
AbdullahELian1 <abodelian28@gmail.com>
2021-09-05 21:12:52 +0300

    rest api

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Blog
+from .serializers import BLogSerializer
 
-# Create your views here.
+class BlogsList(ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BLogSerializer
+
+class BlogsDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BLogSerializer
